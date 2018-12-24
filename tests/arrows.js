e490d10799ef2ed76308f3c9df8c24626dedf688
andrewjohnso7 <andrewjohnso7@yahoo.com>
2018-12-23 20:52:59 -0800

    added up to Higher Order Functions

--- tests/arrows.js
+++ tests/arrows.js
@@ -1,19 +1,64 @@
-// const sum = (a, b) => {
-//   return a + b;
-// };
+/* 
+If you need to reference the actual documentation here's the link via Dec 2018
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+*/
 
 
+// This is just a standard ES5 function
+// TODO: fix eslint to allow for es5 notation as well
 function eSFive (number) {
  return number;
 };
 
+/* 
+This is a standard ES6 function
+note that it is written as a function expression and uses a 'fat arrow'
+keep in mind:
+'fat arrows' === '=>'
+greater than === '>='
+*/
 const eSSix = (number) => {
   return number;
 };
 
-module.exports = {
-  eSFive,
-  eSSix
+/*
+Arrow functions with one parameter do not require that there are parens around the parameter
+*/
+const noParens = string => {
+  return string;
 };
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+/*
+Because developers are trying to be more efficient (or lazy), ES6 adopted the ability to omit the curly braces ({}), and the return keyword if the function body contains one expression
+
+keep in mind:
+no parens,
+no return keyword,
+and no curly braces.
+just a fat arrow and semicolon for style.
+*/
+const noCurlies = string => string;
+
+/*
+This applies to your HIGHER ORDER FUNCTIONS too. (I feel like the shoutcaps are required.)
+
+You'll start to discover that in more complex situations that code readability for those who are not fluent in JS goes down when you go in on the syntatic sugar when it comes to ES6 features, so you'll get to choose if you want to torture them with super clean, but not easy to read code, or make it less clean but highly followable. You're basically a demigod. 
+
+*/
+
+const higherOrderFuncs = (array) => {
+  return array.map(item => item + 100);
+};
+// ˆnot as clean, but I can see that the mapped changes to the original array are being explicity returned to this function.ˆ
+
+const cleanerHigherOrderFuncs = array => array.map(item => item + 100);
+// ˆso clean, sleek, and sexy. Amirite???ˆ
+
+module.exports = {
+  eSFive,
+  eSSix,
+  noParens,
+  noCurlies,
+  higherOrderFuncs,
+  cleanerHigherOrderFuncs
+};
--- tests/arrows.test.js
+++ tests/arrows.test.js
@@ -7,3 +7,29 @@ test('ES5 function', () => {
 test('ES6 Arrow function', () => {
   expect(arrow.eSSix(520)).toBe(520);
 });
+
+test('ES6 Arrow function, with one parameter', () => {
+  expect(arrow.noParens('Look Ma, no parens!')).toBe('Look Ma, no parens!');
+});
+
+test('ES6 Arrow function, with no curlies and one expression', () => {
+  expect(arrow.noCurlies('Look Ma, no curlies!')).toBe('Look Ma, no curlies!');
+});
+
+const original = [100, 250, 400, 900];
+const expected = [200, 350, 500, 1000];
+
+/*
+Had to change the matcher from toBe to toEqual due to pass-by-value vs pass-by-reference.
+toBe tests exact equality,
+toEqual tests values.
+*/
+
+test('ES6 HOF Arrow function, that is less clean, but readable ', () => {
+  expect(arrow.higherOrderFuncs(original)).toEqual(expected);
+});
+
+test('ES6 HOF Arrow function, that is suuuuper clean', () => {
+  expect(arrow.cleanerHigherOrderFuncs(original)).toEqual(expected);
+});
+
