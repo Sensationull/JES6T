c99ec41fad7e2a9c85e3c8d36e708ed5caa1c3b1
andrewjohnso7 <andrewjohnso7@yahoo.com>
2018-12-24 18:33:00 -0800

    added Object destructuring initial tests

--- tests/destruct.js
+++ tests/destruct.js
@@ -1,10 +1,33 @@
 /* Object and Array Destructuring will go here
 
+READ DA DOCS ~ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
 
 */
 
-const holder = name => name;
+// Typically we see objects as literals, like: 
+var composer = {
+  first: 'Pytor Ilyich',
+  last: 'Tchaikovsky',
+  bestSong: 'Waltz Of The Flowers'
+};
+/*
+and so we'd access the specifics via referencing the object's name and the property
+var first = composer.first = 'Pytor Ilyich';
+var last = composer.last = 'Tchaikovsky';
+var bestSong = composer.bestSong = 'Waltz Of The Flowers';
+
+but now, ES6 allows for Object Destructuring which allows us to assign values to given variables
+*/
+// var { first, last, bestSong } = composer;
+
+const returnProperty = name => name;
+
+const destructObj = ({ first, last, bestSong }) => {
+  return first + ' ' + last + ' composed the ' + bestSong;
+};
 
 module.exports = {
-  holder
+  composer,
+  returnProperty,
+  destructObj
 };
--- tests/destruct.test.js
+++ tests/destruct.test.js
@@ -1,5 +1,23 @@
 const destruct = require('./destruct.js');
 
-test('should return the name', () => {
-  expect(destruct.holder('FILL~ME~IN')).toBe('FILL~ME~IN');
+let testComposer = {
+  first: 'Franz',
+  last: 'Liszt',
+  best: 'Hungarian Rhapsody No.2'
+};
+
+var { first, last, best } = testComposer;
+
+
+test('should return the input name', () => {
+  expect(destruct.returnProperty(first)).toBe('Franz');
+});
+
+test('should be able to assign values to given variables', () => {
+  expect(last).toBe('Liszt');
+  expect(best).toBe('Hungarian Rhapsody No.2');
+});
+
+test('should be able to pass destructured values from objects into functions', () => {
+  expect(destruct.destructObj(destruct.composer)).toBe('Pytor Ilyich Tchaikovsky composed the Waltz Of The Flowers');
 });
